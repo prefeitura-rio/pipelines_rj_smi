f4e9857e45e914667a8992342a934d8df04a15b3
Thiago Felipe <thiago.felipe@tutanota.com>
2024-04-12 13:13:49 -0300

    chore: change bucket name

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -11,7 +11,7 @@ class constants(Enum):
     DOCKER_TAG = "AUTO_REPLACE_DOCKER_TAG"
     DOCKER_IMAGE_NAME = "AUTO_REPLACE_DOCKER_IMAGE"
     DOCKER_IMAGE = f"{DOCKER_IMAGE_NAME}:{DOCKER_TAG}"
-    GCS_FLOWS_BUCKET = "rj-orgao-flows"
+    GCS_FLOWS_BUCKET = "datario-public"
 
     ######################################
     # Agent labels
