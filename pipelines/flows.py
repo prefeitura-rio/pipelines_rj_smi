39b6d9ab67f5dae77206fe31178fc1773548c7c3
patriciacatandi <patriciabcatandi@gmail.com>
2024-04-17 15:02:25 -0300

    changing structure to be similiar to smfp

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -3,4 +3,4 @@
 Imports all flows for every project so we can register all of them
 """
 from pipelines.siscob import *  # noqa
-from pipelines.dbt_template import *  # noqa
+from pipelines.templates import *  # noqa
