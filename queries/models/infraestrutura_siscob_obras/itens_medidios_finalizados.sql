aaa32a15e7c241cbaf0d8034592a500d6e7fb95a
d116626 <d116626@gmail.com>
2024-06-18 13:34:13 -0300

    chore: add some queries

--- queries/models/infraestrutura_siscob_obras/itens_medidios_finalizados.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    DISTINCT
-        *
-FROM `rj-smi.infraestrutura_siscob_obras_staging.itens_medidios_finalizados`
--- queries/models/infraestrutura_siscob_obras/orcamento_medicao.sql
+++
@@ -1,4 +0,0 @@
-SELECT
-    DISTINCT
-        *
-FROM `rj-smi.infraestrutura_siscob_obras_staging.orcamento_medicao`
